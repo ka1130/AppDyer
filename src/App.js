7cd054d3f86bfb6deab5c963b0aafa403eb68e10
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-11-10 16:35:05 +0100

    Add autocomplete component with HTML5 datalist tag

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
+import { colors } from 'data';
+
 import styles from './App.module.scss';
 
-class App extends Component {
+class Autocomplete extends Component {
   render() {
     return (
-      <div className={styles.app}>
+      <>
+        <input
+          list="colors"
+          className={styles.colorInput}
+          placeholder="Start typing the color..."
+          onChange={this.props.onChange}
+        />
+        <datalist id="colors">
+          {colors().map((color, i) => <option key={i} value={color.name}>{color.name}</option>)}
+        </datalist>
+      </>
+    );
+  }
+}
+
+class App extends Component {
+  state = { color: '' }
+
+  onInputChange = e => this.setState({ color: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className={styles.app} style={{background: this.state.color}}>
         <h3>Choose background color</h3>
-        <form className={styles.colorPickerForm}>
-          <input
-            type="text"
-            className={styles.colorInput}
-            placeholder="Start typing the color..."
-          />
+        <form
+          className={styles.colorPickerForm}
+          onSubmit={this.onSubmit}
+        >
+          <Autocomplete onChange={this.onInputChange}/>
           <button
             type="submit"
             className={styles.submitColor}
+            onClick={this.onSubmit}
           >
-            Submit
+            Accept
           </button>
         </form>
+        <p>Color name: {this.state.color}</p>
       </div>
     );
   }
