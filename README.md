c8f772cd982c893e790cfb3c5862112213871c36
Kamila Matla-Tomczyk <ka1130@o2.pl>
2018-11-12 19:03:56 +0100

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# AppDyer
-A simple React npm lib to pick up you app's background color
-
-# Demo
-Demo of the app: [http://ka1130.github.io/AppDyer](http://ka1130.github.io/AppDyer)
---
+++ precache-manifest.9db72bb9da34e0e7eed412b43aa2df75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdddff261563cb8bc70f",
+    "url": "/AppDyer/static/js/runtime~main.bdddff26.js"
+  },
+  {
+    "revision": "d3e027ab1a254693f334",
+    "url": "/AppDyer/static/js/main.d3e027ab.chunk.js"
+  },
+  {
+    "revision": "91ea072c0dc511c98d6c",
+    "url": "/AppDyer/static/js/1.91ea072c.chunk.js"
+  },
+  {
+    "revision": "d3e027ab1a254693f334",
+    "url": "/AppDyer/static/css/main.ee60fff4.chunk.css"
+  },
+  {
+    "revision": "9712e58bd1dbc0c41451ae770f98d1ee",
+    "url": "/AppDyer/index.html"
+  }
+];
